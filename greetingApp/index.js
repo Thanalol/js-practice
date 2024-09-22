db4270070a90904b34b19183543233cc4ac46bd9
Thanalol <developante@gmail.com>
2024-09-22 23:48:10 +0200

    Added another user greeting but I will leave this project now. Onto the next Odin chapter.

--- greetingApp/index.js
+++ greetingApp/index.js
@@ -2,6 +2,7 @@ const form = document.querySelector('.form');
 const resultDisplay = document.getElementById('resultDisplay');  
 const gagDisplay = document.getElementById('gagDisplay');
 const changeColor = document.getElementById('changeColor');
+const userGreet = document.getElementById('userGreet');
 
 
 form.addEventListener('submit', function(event) {
@@ -17,9 +18,11 @@ form.addEventListener('submit', function(event) {
    
     const message = `Hello ${name} ${surname}, a ${age}-year-old from ${hometown}! 
     It's awesome that you are from ${nationality}, and you love listening to ${music}.`;
+    const greeting = `Oh, hey ${name}! Nice to have you here!`
 
   
     resultDisplay.textContent = message;
+    userGreet.textContent = greeting;
 
     if(age >= 30){
         gagDisplay.textContent = "Damn! You're fucking old."
