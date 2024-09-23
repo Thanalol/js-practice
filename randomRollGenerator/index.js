72b4137a4c982803a8326a2823e736e8d205c10c
Thanalol <developante@gmail.com>
2024-09-23 21:59:47 +0200

    Realized my mystake, fixed my syntax and got the score working. Reset button next.

--- randomRollGenerator/index.js
+++ randomRollGenerator/index.js
@@ -3,57 +3,57 @@ const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 
-const playerScore = document.getElementById("playerScore");
-const computerScore = docuument.getElementById("computerScore");
-const tieScore = document.getElementById("tieScore");
+const playerScoreDisplay = document.getElementById("playerScore");
+const computerScoreDisplay = document.getElementById("computerScore");
+const tieScoreDisplay = document.getElementById("tieScore");
+
+
+let playerWin = 0;
+let computerWin = 0;
+let tieWin = 0;
 
 function getRandomNumber() {
-    return Math.floor(Math.random()* 100) + 1;
+    return Math.floor(Math.random() * 100) + 1;
 }
 
 function startGame() {
-    
     const playerRoll = getRandomNumber();
     const computerRoll = getRandomNumber();
     let result;
 
-    if (playerRoll === computerRoll){
-        result = "It's a tie!!!"
+    if (playerRoll === computerRoll) {
+        result = "It's a tie!!!";
         resultDisplay.style.color = "black";
-    } else if (playerRoll > computerRoll){
-        result = "Player wins!"
+    } else if (playerRoll > computerRoll) {
+        result = "Player wins!";
         resultDisplay.style.color = "green";
     } else {
-        result ="Computer wins!"
+        result = "Computer wins!";
         resultDisplay.style.color = "red";
     }
 
-    if ( result === "Player wins!"){
-        
-    }
+    
+    updateScore(result);
 
     playerDisplay.textContent = `Player roll: ${playerRoll}`;
     computerDisplay.textContent = `Computer roll: ${computerRoll}`;
     resultDisplay.textContent = result;
+}
 
-function updateScore(result){
 
-    let playerWin = 0;
-    let computerWin = 0;
-    let tieWin = 0;
-
-    if (result === "player wins!"){
+function updateScore(result) {
+    if (result === "Player wins!") {
         playerWin++;
     } else if (result === "Computer wins!") {
         computerWin++;
-    } else {
+    } else if (result === "It's a tie!!!") {
         tieWin++;
     }
 
-    playerScore.textContent
-
-}
+    
+    playerScoreDisplay.textContent = `You have ${playerWin} Points!`;
+    computerScoreDisplay.textContent = `Computer has ${computerWin} Points!`;
+    tieScoreDisplay.textContent = `Ties: ${tieWin}`;
 }
 
 startButton.addEventListener('click', startGame);
-
